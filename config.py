82c15ba0b9a56f15cfeccdd6ebb9e662795e78c5
bskim <bskim@bskimui-MacBookPro.local>
2021-01-05 19:22:46 +0900

    bug fix

--- config.py
+++ config.py
@@ -320,15 +320,15 @@ def get_args():
         args.axial_pos_shape = [16, 32]
         # args.finetune_train_batch_size = 64
         # args.finetune_test_batch_size = 256
-        args.finetune_train_batch_size = 16
-        args.finetune_test_batch_size = 32
+        args.finetune_train_batch_size = 8
+        args.finetune_test_batch_size = 16
         args.finetune_update_steps = 20
     elif args.max_seq_size == 1024:
         args.axial_pos_shape = [32, 32]
         # args.finetune_train_batch_size = 64
         # args.finetune_test_batch_size = 128
-        args.finetune_train_batch_size = 8
-        args.finetune_test_batch_size = 16
+        args.finetune_train_batch_size = 4
+        args.finetune_test_batch_size = 8
         args.finetune_update_steps = 20
     elif args.max_seq_size == 2048:
         args.axial_pos_shape = [32, 64]
